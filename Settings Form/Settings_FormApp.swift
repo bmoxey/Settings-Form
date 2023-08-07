adfa57f8010922be1bf78e0e2b831f11bdb2e95f
bmoxey <bmoxey@gmail.com>
2023-08-08 06:45:27 +1000

    Initial Commit

---
+++ Settings Form/Settings_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Settings_FormApp.swift
+//  Settings Form
+//
+//  Created by Brett Moxey on 8/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct Settings_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
